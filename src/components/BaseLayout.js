fe54a7cfbd609ec0fbe951456b8b0d008001788f
Being-Nayem <nayemsarwar12345@gmail.com>
2023-03-24 02:21:13 +0600

    error solved

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -18,14 +18,14 @@ export default function BaseLayout() {
    }
 
    useEffect(() => {
-      let detectedDarkMode = eval(localStorage.getItem('darkMode'));
-
-      if (detectedDarkMode) {
-         setDarkMode(detectedDarkMode)
+      let detectedDarkMode = JSON.parse(localStorage.getItem('darkMode'));
+  
+      if (detectedDarkMode !== null) {
+          setDarkMode(detectedDarkMode);
       } else {
-         localStorage.setItem('darkMode', 'false')
+          localStorage.setItem('darkMode', 'false');
       }
-   }, [])
+  }, []);
 
    return (
       <Box className={darkMode ? Style.dark : Style.light}>
